d30136f2eb40136e99ac572c71f411449eafdb90
Richard <rparkerjr77@gmail.com>
2019-06-18 16:41:27 -0700

    tpon.sh, tpoff.sh
    Added scripts I use to enable/disable the touchpad since there is no hardware of software equivalent.

---
+++ tpoff.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#Disable touchpad
+# xinput --disable "touchpadnamehere"
+# xinput --disable 13  # if using id
+
+xinput --disable "SynPS/2 Synaptics TouchPad"
---
+++ tpon.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#Enable touchpad
+# xinput --enable "touchpadnamehere"
+# xinput --enable 13  # if using id
+
+xinput --enable "SynPS/2 Synaptics TouchPad"
