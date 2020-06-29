9c494ce974b56c7ce92073112768ae015eed765b
Parker <rparkerjr77@gmail.com>
2020-06-29 15:59:05 -0700

    Minor update

--- tpoff.sh
+++ tpoff.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 #Disable touchpad
 # xinput --disable "touchpadnamehere"
 # xinput --disable 13  # if using id
 
 xinput --disable "SynPS/2 Synaptics TouchPad"
+echo "Touchpad disabled"
--- tpon.sh
+++ tpon.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 #Enable touchpad
 # xinput --enable "touchpadnamehere"
 # xinput --enable 13  # if using id
 
 xinput --enable "SynPS/2 Synaptics TouchPad"
+echo "Touchpad enabled"
